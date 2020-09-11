1f0d8509de55d6e7e85238bf460452aa5add10ea
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-09-11 19:12:28 +0200

    add first styles and variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getLocation, getWeatherLocation } from './services/weather/api';
 import WeatherInfo from './components/WeatherInfo/WeatherInfo';
-import './assets/stylesheets/App.css';
+import './assets/stylesheets/App.scss';
 
 const App = () => {
   const [locationWeather, setLocationWeather] = useState({});
--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 const WeatherInfo = ({ temperature }) => {
   return (
-    <div>
-      <h1>{temperature}</h1>
-      <span>Shower</span>
-      <span>Location</span>
-    </div>
+    <aside className="weatherInfo">
+      <div>
+        <buton className="weatherInfo__searchBtn">Search for places</buton>
+      </div>
+      <div className="weatherInfo__imgContainer">
+        <img src="" alt="" className="weatherInfo__img"></img>
+      </div>
+      <h1 className="weatherInfo__temperature">{temperature}</h1>
+      <span className="weatherInfo__state">Shower</span>
+      <div className="weatherInfo__dateContainer">
+        <span>Today</span>
+        <span>Date</span>
+      </div>
+      <span className="weatherInfo__location">Location</span>
+    </aside>
   );
 };
 
